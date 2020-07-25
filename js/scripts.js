576f8c5c9e6f1834ac0dfb91e3c5ea8fdacf3127
João Pedro Maciel <joomaciiel2@outlook.com>
2020-07-25 19:02:56 -0300

    header

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//========================================================
+// Marker do menu selecionado.
+var marker = document.querySelector('#marker');
+var item = document.querySelectorAll('nav ul li a');
+
+function indicator(e){
+    marker.style.left = e.offsetLeft+"px";
+    marker.style.width = e.offsetWidth+"px";
+}
+
+item.forEach(link => {
+    link.addEventListener('click', (e)=>{
+        indicator(e.target);
+    })
+})
+
+//========================================================
