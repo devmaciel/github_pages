92edba75328d91563c61ba72f814bd30a8a25b62
João Pedro Maciel <joomaciiel2@outlook.com>
2020-07-27 00:25:05 -0300

    responsive

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,19 @@ item.forEach(link => {
 })
 
 //========================================================
+// MOBILE
+$('nav.mobile').click(function(){
+    var listaMenu = $('nav.mobile ul');
+    if(listaMenu.is(':hidden') == true){
+        var icone = $('.botao-menu-mobile').find('i');
+        icone.removeClass('fa-bars');
+        icone.addClass('fa-times');
+        listaMenu.slideToggle();
+    }
+    else{
+        var icone = $('.botao-menu-mobile').find('i');
+        icone.removeClass('fa-times');
+        icone.addClass('fa-bars');
+        listaMenu.slideToggle();
+    }
+})
